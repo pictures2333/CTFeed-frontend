ddfdbef4189e5314bcf97a97bcba279d9fa37c24
pictures2333 <m0987529191@gmail.com>
2026-02-07 17:06:57 +0800

    feat: icon

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const APP_CONFIG = {
   apiBaseUrl: import.meta.env.VITE_API_BASE_URL ?? "",
   appTitle: "ICEDTEA CTF bot",
-  logoUrl: "https://avatars.githubusercontent.com/u/179367473",
+  logoUrl: "/logo.png",
 };
